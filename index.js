f4848ee07aabc6cfc6b23d5572f4dc93a0e1ff29
sarah <sarachamorro@gmail.com>
2020-09-04 18:49:29 -0300

    Add about me section

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
   const nav = document.getElementById('nav');
+  const dropDowns = document.getElementsByClassName('about-skill');
 
   window.document.addEventListener('scroll', e => {
     if (e.target.scrollingElement.scrollTop > 35) {
@@ -7,5 +8,17 @@ window.onload = () => {
     } else {
       nav.classList.remove('shadow-1');
     }
+  });
+  document.querySelector('#null').checked = true;
+
+  Array.from(dropDowns).forEach(drop => {
+    drop.addEventListener('click', () => {
+      let dropInput = document.getElementById(drop.attributes.for.nodeValue);
+      if (dropInput.checked) {
+        setTimeout(() => {
+          document.querySelector('#null').click();
+        })
+      }
+    });
   });
 };
