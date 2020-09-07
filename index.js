53ff582129d4542f7d6282102b11c0db356b8b6b
sarah <sarachamorro@gmail.com>
2020-09-07 14:51:52 -0300

    Add contact form and make it responsive

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 window.onload = () => {
   const nav = document.getElementById('nav');
   const dropDowns = document.getElementsByClassName('about-skill');
+  const navExpand = document.getElementById('nav-expand');
+  const navClose = document.getElementById('nav-close');
+  const navMobile = document.getElementById('nav--mobile');
+  const mobileLinks = document.getElementsByClassName('mobile-link');
 
   window.document.addEventListener('scroll', e => {
     if (e.target.scrollingElement.scrollTop > 35) {
@@ -10,6 +14,20 @@ window.onload = () => {
     }
   });
   document.querySelector('#null').checked = true;
+
+  navExpand.addEventListener('click', () => {
+    navMobile.classList.remove('hide');
+  });
+
+  navClose.addEventListener('click', () => {
+    navMobile.classList.add('hide');
+  });
+
+  Array.from(mobileLinks).forEach(link => {
+    link.addEventListener('click', () => {
+      navMobile.classList.add('hide');
+    });
+  })
 
   Array.from(dropDowns).forEach(drop => {
     drop.addEventListener('click', () => {
